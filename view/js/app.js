9f7f1a7703fecaa0d5b39942c895c7846cab6acf
Jefferxx <93774436+Jefferxx@users.noreply.github.com>
2025-07-02 00:20:50 -0500

    Creados archivos de FrontEnd, HTML, CSS, JS

---
+++ view/js/app.js
@@ -0,0 +1,21 @@
+async function cargarLibros(orden){
+    const res= await fetch('../public/apiLibros.php?orden=${orden}');
+    const libros= await res.json();
+
+    const container = document.getElementById("contenedorTarjetas");
+    container.innerHTML="";
+
+    libros.forEach(libro=>{
+        const div=document.createElement("div");
+        div.classList.add("card");
+        div.innerHTML= `
+            <h3>${libro.titulo}</h3>
+            <p><strong>Género:</strong> ${libro.genero}</p>
+            <p><strong>Precio:</strong> $${libro.precio}</p>
+            <p><strong>Páginas:</strong> ${libro.paginas}</p>
+            <p><strong>Año:</strong> ${libro.anio_publicacion}</p>
+        `;
+        container.appendChild(div);
+    });
+}
+cargarLibros('asc'); //cargar por defecto ascendente
